1936640d76b26e21e249b98879b3f740e8e60c42
AlexandruRusu23 <specialone47@gmail.com>
2016-10-25 13:37:21 +0300

    haskell

--- Haskell-Labs/Lab3/lab3.hs
+++ Haskell-Labs/Lab3/lab3.hs
@@ -10,7 +10,8 @@ import Test.QuickCheck
 
 -- 1.
 rotate :: Int -> [Char] -> [Char]
-rotate = undefined
+rotate n xs = if n>=0 && n<length xs then concat[drop n xs, take n xs]
+	else error "EROARE"
 
 -- 2.
 prop_rotate :: Int -> String -> Bool
@@ -20,34 +21,35 @@ prop_rotate k str = rotate (l - m) (rotate m str) == str
 
 -- 3. 
 makeKey :: Int -> [(Char, Char)]
-makeKey = undefined
+makeKey n = zip ['A'..'Z'] (rotate n ['A'..'Z'])
 
 -- 4.
 lookUp :: Char -> [(Char, Char)] -> Char
-lookUp = undefined
+lookUp c xc = if length l > 0 then head l else c
+			where l = [b | (a,b) <- xc, c == a]
 
 -- 5.
 encipher :: Int -> Char -> Char
-encipher = undefined
+encipher n c = lookUp c (makeKey n) 
 
 -- 6.
 normalize :: String -> String
-normalize = undefined
+normalize str = [toUpper c | c <- str, isAlpha c || isDigit c]
 
 -- 7.
 encipherStr :: Int -> String -> String
-encipherStr = undefined
+encipherStr n str = [ encipher n x | x <- normalize str]
 
 -- 8.
 reverseKey :: [(Char, Char)] -> [(Char, Char)]
-reverseKey = undefined
+reverseKey xs = [ (b,a) | (a,b) <- xs ]
 
 -- 9.
 decipher :: Int -> Char -> Char
-decipher = undefined
+decipher n c = lookUp c ( reverseKey (makeKey n) )
 
 decipherStr :: Int -> String -> String
-decipherStr = undefined
+decipherStr n str = [ decipher n c | c <- str ]
 
 -- 10.
 prop_cipher :: Int -> String -> Bool
